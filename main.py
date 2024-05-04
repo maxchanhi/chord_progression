a88683d33d3326bc48e913e266e365a89265b8b7
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-04 23:00:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from notation import fun_emoji_list
+import random
+from question import lilypond_generation,question_generation,display_options,chord_accompany,audio_generation
+from progression import main_generation,key_generation
+from streamlit_extras.let_it_rain import rain
+ss=st.session_state
+def new_question(choosen_range="Major"):
+    choosen_key=key_generation(choosen_range)
+    ss["question_data"] = main_generation(choosen_key)
+    score_melody = ss["question_data"]["melody"]
+    correct_option = ss["question_data"]["chord"]
+    key_signature = ss["question_data"]["key"]
+    accom_part=chord_accompany(key_signature,correct_option)
+    lilypond_generation(score_melody, "question",key_signature,accom_part)
+    correct_index,options=question_generation(correct_option)
+    options= display_options(options)
+    ss["button_pressed"]=False
+    return correct_index,options,key_signature
+def button_pressed():
+    ss["button_pressed"]=True
+    return ss["button_pressed"]
+st.set_page_config(page_title="Harmonise melody")
+
+st.title("Identify the Correct Harmonic Pattern")
+choosen_range = st.multiselect("Select the keys:",["Major","minor"],default=["Major"])
+
+if choosen_range is None:
+    st.warning("Please select a key.")
+new_score = st.button("Generate Score")
+
+if new_score and choosen_range:
+    ss["question_data"]=new_question(choosen_range)
+if "question_data" not in ss:
+    ss["question_data"]=new_question(choosen_range)
+
+correct_index,options,key_signature=ss["question_data"]
+st.image("static/cropped_score_question.png", use_column_width=True)
+user_ans = st.radio(f"Select the correct harmonic pattern in {key_signature}:",options)
+
+if st.button("Submit",on_click=button_pressed,disabled=ss["button_pressed"]):
+    if options.index(user_ans) == correct_index:
+        emoji= random.choice(fun_emoji_list)
+        st.success(f"Correct!{emoji}")
+        rain(emoji=emoji,animation_length=1)
+        st.balloons()
+        audio_generation()
+        st.audio("static/question.mp3")
+    else:
+        st.warning(f"Incorrect! The answer is {options[correct_index]}")
+
+
+
+
+
---
+++ question.py
@@ -0,0 +1,118 @@
+from progression import main_generation,major_roman,major_tonal_triad
+
+import random
+import subprocess
+from PIL import Image
+import os
+
+def question_generation(ans_prograssion):
+    options = []
+    while len(options) < 5:
+        bar=[]
+        while len(bar) <4 :
+            bar.append(random.choice(major_roman))
+        if bar != ans_prograssion and bar not in options:
+            options.append(bar)
+        else:
+            bar.clear()
+    options.append(ans_prograssion)
+    random.shuffle(options)
+    correct_index = options.index(ans_prograssion)
+    return correct_index,options
+
+
+def display_options(options):
+    cleaned_progressions = ['-'.join(progression) for progression in options]
+    return cleaned_progressions
+
+
+def format_melody(melody):
+  formatted = []
+  for note in melody:
+      note = note.replace("'", "").replace(',', '').replace('[', '').replace(']', '')
+      formatted.append(note)
+  return ' '.join(formatted)
+
+def key_signature(key="F-sharp minor"):
+    letter,quality=key.split()
+    if "sharp" in letter:
+        accidental="s"
+        letter=letter[0]
+    elif "flat" in letter:
+        accidental="f"
+        letter=letter[0]
+    else:
+        accidental=""
+    return f"{letter.lower()}{accidental} \{quality}"
+
+def chord_accompany(key="C major",chords=["ii","IV","V","I"]):
+    scale, triad_dic = major_tonal_triad(key)
+    chord_progression = []
+    for chord in chords:
+        chord_notes = triad_dic[chord]
+        chord_str = "<" + " ".join(chord_notes) + ">1"
+        chord_progression.append(chord_str)
+    return " ".join(chord_progression)
+
+def lilypond_generation(melody, name, key,accompany):
+    score_meldoy = []
+    for bar in melody:
+        score_meldoy.extend(bar)
+    lilypond_score = f"""
+\\version "2.24.1"  
+\\header {{
+  tagline = "" \\language "english"
+}}
+
+#(set-global-staff-size 26)
+\\score {{
+
+    \\fixed c' {{ 
+    \\key {key_signature(key)}
+
+      {format_melody(score_meldoy)}
+      \\bar "|"
+    }}
+    \\layout {{
+      indent = 0\\mm
+      ragged-right = ##f
+      \\context {{
+        \\Score
+        \\remove "Bar_number_engraver"
+      }}
+    }}
+}}
+\\score {{\\new StaffGroup <<
+     \\new Staff \\fixed c' {{
+      {format_melody(score_meldoy)}
+      \\bar "|"
+    }}
+    \\new Staff \\fixed c {{
+      {accompany}
+      \\bar "|"
+    }}>>
+    \\midi {{ }}
+}}
+"""
+
+    with open('score.ly', 'w') as f:
+        f.write(lilypond_score)
+
+    # Generate PNG image and MIDI file
+    subprocess.run(['lilypond', '-dpreview', '-dbackend=eps', '--png', '-dresolution=300', '--output=static/score', 'score.ly'],
+                   check=True)
+    with Image.open('static/score.png') as img:
+        width, height = img.size
+        crop_height = height
+        crop_rectangle = (0, 75, width, crop_height / 5.5)
+        cropped_img = img.crop(crop_rectangle)
+
+        os.makedirs('static', exist_ok=True)
+        cropped_img.save(f'static/cropped_score_{name}.png')
+
+    return f'static/cropped_score_{name}.png'
+
+
+def audio_generation():
+    subprocess.run(['fluidsynth', '-ni', '/Users/chakhangchan/Documents/VS_code/Music_theory_app/melody_key/GeneralUser/Yamaha-Grand-Lite-SF-v1.1.sf2', 'static/score.midi', '-F', f'static/question.mp3', '-r', '44100'],
+               check=True)
